f332d58b9d19753db6163369f8061d00ea4acac7
chienyuanhung <chien.yuanhung@gmail.com>
2019-08-14 19:33:11 -0700

    change in bubble.js

--- bubble.js
+++ bubble.js
@@ -1,6 +1,26 @@
  d3.json("top_movies_updated.json", function(error, data){
-     console.log(data);
+    //  console.log(data);
+     var actors = []
+     var movies = []
      data.forEach(function(d){
-         console.log(d.Title)
+         for (var i = 0; i<d.Cast_list.length; i++) {
+             actors.push(d.Cast_list[i])
+             movies.push(d.Title)
+         }  
      })
+    //  console.log(actors)
+    //  console.log(movies)
+
+     var actorMovies = new Object()
+     for (var j = 0; j<actors.length; j++){
+         if (actors[j] in Object.keys(actorMovies)){
+            actorMovies[actors[j]].push(movies[j])
+         }
+         else{
+             actorMovies[actors[j]] = []
+             actorMovies[actors[j]].push(movies[j])
+         }
+         
+     }
+     console.log(actorMovies.keys().length)
 })
