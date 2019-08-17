e3788f05a7431d4b735a40325fbace3ced470374
Yara1994 <nick.yara1994@gmail.com>
2019-08-17 12:12:57 -0700

    Changed directions

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,34 +1,7 @@
 // Multi Bar Chart (budget/Profit)
 
-d3.json("../top_movies_updated.json", function(error, myData){
+d3.csv("data/budget_gross.csv", function(error, myData){
     console.log(myData)
-
-    var budget = []
-    var gross = []
-    var title = []
-
-    myData.forEach(function(d){
-        title.push(d.Title)
-        budget.push(d.Budget)
-        gross.push(d.Gross_world_wide)
-        
-
-
-    })
-    console.log(title)
-    console.log(budget)
-    console.log(gross)
-
-
-
-
-
-
-
-
-
-
-
 })
 
 
