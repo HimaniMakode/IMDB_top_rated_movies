9eaf24d67682bc8d8a81152802f565c03e5cf2cf
chienyuanhung <chien.yuanhung@gmail.com>
2019-08-20 14:18:07 -0700

    new app.py for flask

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+import scrape_imdb
 
 # create instance of Falsk app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/IMDB_app"
@@ -12,8 +12,7 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    marsdata = mongo.db.marsdata.find_one()
-    return render_template("index.html", imdbdata = imdbdata)
+    return render_template("index.html")
 
 @app.route("/scrape")
 def scrape():
@@ -21,7 +20,6 @@ def scrape():
     idata = scrape_imdb.scrape_all()
     imdbdata.update({}, idata, upsert=True)
     return redirect("/", code=302)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
